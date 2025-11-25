da4e2a33f781e749d873446155633a721380154a
Yo.Pitchayakorn <pitchayakorn.s@clicknext.com>
2025-11-26 00:00:31 +0700

    remove unused CORS import from server setup

--- src/server.ts
+++ src/server.ts
@@ -1,7 +1,6 @@
 import express from 'express';
 import http from 'http';
 import { Server } from 'socket.io';
-import cors from 'cors';
 
 const app = express();
 const server = http.createServer(app);
